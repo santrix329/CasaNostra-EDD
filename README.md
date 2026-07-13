2ee77290664ee4c59d794a93261a477af65e7721
santrix329 <tiagoalexey17@gmail.com>
2026-07-12 23:13:34 -0400

    chore: configurar estructura de carpetas bin y src con datos de prueba

---
+++ README.md
@@ -0,0 +1,2 @@
+# Casa Nostra - Estructura de Datos Dinámica
+Prototipo para la gestión de sucesión de la familia de la mafia italiana
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Iniciando sistema de la familia" << endl;
+    return 0;
+}
